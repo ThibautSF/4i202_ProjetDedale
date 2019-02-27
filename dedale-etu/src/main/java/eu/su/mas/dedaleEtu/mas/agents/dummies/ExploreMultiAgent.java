5c782437b5b5cd1daa458ac77f3d89953d045cdd
nesrineBI <46285177+nesrineBI@users.noreply.github.com>
2019-02-27 11:49:54 +0100

    Update ExploreMultiAgent.java

--- dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/agents/dummies/ExploreMultiAgent.java
+++ dedale-etu/src/main/java/eu/su/mas/dedaleEtu/mas/agents/dummies/ExploreMultiAgent.java
@@ -7,9 +7,11 @@ import java.util.List;
 import eu.su.mas.dedale.mas.AbstractDedaleAgent;
 import eu.su.mas.dedale.mas.agent.behaviours.startMyBehaviours;
 import eu.su.mas.dedaleEtu.mas.behaviours.ExploMultiBehaviour;
+import eu.su.mas.dedaleEtu.mas.behaviours.ReceiveMessageBehaviour;
 import eu.su.mas.dedaleEtu.mas.behaviours.SayHello;
 import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
 import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.FSMBehaviour;
 
 /**
  * ExploreSolo agent. 
@@ -48,11 +50,33 @@ public class ExploreMultiAgent extends AbstractDedaleAgent {
 		 * 
 		 ************************************************/
 		
-		lb.add(new ExploMultiBehaviour(this,this.myMap,this.myGraph));
-		lb.add(new SayHello(this));
+		//lb.add(new ExploMultiBehaviour(this,this.myMap,this.myGraph));
+		//lb.add(new SayHello(this));
 		//lb.add(new ReceiveMessageBehaviour(this));
 		
+		FSMBehaviour fsm = new FSMBehaviour(this) {
+			public int onEnd() {
+				System.out.println("FSM behaviour terminé");
+				myAgent.doDelete();
+				return super.onEnd();
+			}
+
+		};
+		//definiton des etats
+		fsm.registerFirstState	(new ExploMultiBehaviour(this,this.myMap,this.myGraph),"A");
+		fsm.registerState(new ReceiveMessageBehaviour(this),"B");
+		//fsm.registerState(new SendMapBehaviour(this),"C");
+		fsm.registerLastState(new SayHello(this),"D");
 		
+		//definition des transaction
+		//fsm.registerDefaultTransition("A","B");
+		fsm.registerTransition("A","B",1); // si boite n'est pas vide
+		fsm.registerTransition("A","D",2); //si boite est vide 
+		fsm.registerTransition("D","A",1); //si y'a personne à ma portée
+		fsm.registerTransition("D","B",2); //si y'a quelq'un à ma portée
+				
+		lb.add(fsm);
+	
 		/***
 		 * MANDATORY TO ALLOW YOUR AGENT TO BE DEPLOYED CORRECTLY
 		 */
